648c80f60f26b8b6a5975e3d1e751c82053fd65c
darialevina <dlevina96@yandex.ru>
2019-05-26 22:24:59 +0300

    Внесены правки
    Теперь data[MAX_RECORDS] не глобальная, в куче; #define -> const int; добавлена функция проверки порядка отсортированных данных и тесты.

--- LAB_1/main.cpp
+++ LAB_1/main.cpp
@@ -2,7 +2,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-#define LINE_LEN 128
+const int LINE_LEN = 128;
 
 int readData(const char* filepath, int* data, int maxRecords) {
 	FILE* file = fopen(filepath, "r");
@@ -12,7 +12,7 @@ int readData(const char* filepath, int* data, int maxRecords) {
 	int numRecords = 0;
 
 	while (fgets(line, LINE_LEN, file) != NULL &&
-		   numRecords < maxRecords) {
+		numRecords < maxRecords) {
 		*data = atoi(line);
 		data++;
 		numRecords++;
@@ -50,12 +50,22 @@ void sort(int* data, int len) {
 	}
 }
 
-const char* inputPath  = "inputA.txt";
+int isAscending(int* data, int len) {
+	for (int i = 1; i < len; i++) {
+		if (data[i] < data[i - 1]) { return 0; }
+	}
+
+	return 1;
+}
+
+const char* inputPath = "inputA.txt";
 const char* outputPath = "output.txt";
-#define MAX_RECORDS 1024
-int data[MAX_RECORDS];
+const int MAX_RECORDS = 1024;
 
 int main() {
+
+	int* data = new int[MAX_RECORDS];
+
 	printf("LAB 1\n");
 	printf("Reading file...\n");
 
@@ -81,6 +91,28 @@ int main() {
 		return 0;
 	}
 	printf("Writing file OK\n");
+
+	delete[] data;
+
+	// *** *** ***
+
+	printf("Testing... \n");
+
+	int testDataA[] = { -3,  2,  1,  0,   1,  2,  3, };
+	int testDataB[] = { 7,  6,  5,  4,   3,  2,  1, -10, -4 };
+	int testDataC[] = { 3, 56, 18, -7, 247,  0, -1, };
+	int testDataD[] = { 0xDEADBEEF, };
+	int testDataE[] = { 1,  2,  3 };
+	int testDataF[] = { 7,  8,  7,  8,   7,  8,  7,   8, 7, 8, 7, 8, 7, 8 };
+
+	sort(testDataA, sizeof(testDataA) / sizeof(int)); printf("TEST A : %s.\n", isAscending(testDataA, sizeof(testDataA) / sizeof(int)) ? "OK" : "FAIL");
+	sort(testDataB, sizeof(testDataB) / sizeof(int)); printf("TEST B : %s.\n", isAscending(testDataB, sizeof(testDataB) / sizeof(int)) ? "OK" : "FAIL");
+	sort(testDataC, sizeof(testDataC) / sizeof(int)); printf("TEST C : %s.\n", isAscending(testDataC, sizeof(testDataC) / sizeof(int)) ? "OK" : "FAIL");
+	sort(testDataD, sizeof(testDataD) / sizeof(int)); printf("TEST D : %s.\n", isAscending(testDataD, sizeof(testDataD) / sizeof(int)) ? "OK" : "FAIL");
+	sort(testDataE, sizeof(testDataE) / sizeof(int)); printf("TEST E : %s.\n", isAscending(testDataE, sizeof(testDataE) / sizeof(int)) ? "OK" : "FAIL");
+	sort(testDataF, sizeof(testDataF) / sizeof(int)); printf("TEST F : %s.\n", isAscending(testDataF, sizeof(testDataF) / sizeof(int)) ? "OK" : "FAIL");
+
+	printf("Testing OK\n");
 
 	getchar();
 	return 0;
